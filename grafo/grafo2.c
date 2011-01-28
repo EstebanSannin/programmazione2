a9ed6e787256937211b256335a0c0545c2559add
Esteban Sannin <stefanoviola@sannioglug.org>
2011-01-28 21:09:54 +0100

    added DFS function

--- grafo/grafo2.c
+++ grafo/grafo2.c
@@ -1,3 +1,13 @@
+/*
+ *    Author: Stefano Viola aka estebanSannin
+ *   Contact: stefanoviola@sannioglug.org
+ * Home Page: http://esteban.homelinux.org
+ *
+ *      NAME: Analisi grafo - profondita` e ampiezza
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -55,7 +65,6 @@ int add(const int from, const int to, const char *descr){
 }
 
 /*rimuove un nodo relativo*/
-
 int remove_n(const int from){
 	Record *curr;
 	Record *prev;
@@ -116,7 +125,7 @@ void ampiezza(int from, int *vect){
 	curr = list->next;
 	static int memory[MAX_NODE];
 	static int i = 1;
-	
+
 	memory[from]=1;
 	static int count = 0;
 
@@ -138,6 +147,59 @@ void ampiezza(int from, int *vect){
 	}
 }
 
+
+
+
+void profondita(int from, int *vect){
+	Record *curr;
+	curr = list->next;
+	static int i = 1; 
+	static int z=0;
+	static int mem[MAX_NODE];
+	static int v2[MAX_NODE];
+	int next;
+	int flag = 0;
+	mem[from]=1;
+
+	//ciclo tutto il grafo
+	while(curr){
+		//controllo se trovo il nodo passato
+		if(curr->from == from){
+			//controllo per prelevare solo il primo nodo destinazione corrispondente
+			if(flag!=1){
+				next=curr->to;
+				flag = 1;
+			}else{			//altrimenti tutti gli altri nodi relativi li accumulo in un vettore v2
+				v2[z]=curr->to;
+				z++;
+			}
+		}
+		curr = curr->next; //passo al nodo successivo del grafo
+	}
+	flag = 0;	//azzero il flag che identificava il primo nodo destinatario
+
+	//controllo se il nodo successivo da analizzare e' nullo oppure e' gia' uscito
+	if(mem[next]==1 || next==0){
+		//controllo che nel vettore di accumulo ci sia almeno un elemento disponibile
+		//se si esso diventera' il next e verra' eliminato dal vettore accumulo
+		if(v2[z-1] != 0){
+			next=v2[z-1];
+			v2[z-1]=0;
+			z=z-1;
+		}else{			//altrimenti se il vettore di accumulo e' vuoto ho terminato l'algoritmo
+			return 0;
+		}
+	}
+	//ora se in next contiene un nodo che non e' uscito lo aggiungo all'array finale, altrimenti 
+	if(mem[next]!=1){
+		vect[i]=next;
+		i++;
+	}
+	profondita(next,vect);
+}
+
+
+
 int init(){
 	Record *r;
 	r = malloc(sizeof(Record));
@@ -157,31 +219,40 @@ int main(){
 	add(1, 2, "Nodo 1 ---> 2");
 	add(1, 3, "Nodo 1 ---> 3"); // aggiungo i nodi del grafo alla lista
 	add(3, 4, "Nodo 3 ---> 4");
+	add(3, 7, "Nodo 4 ---> 1");
+	add(2, 6, "Nodo 4 ---> 1");
 	add(4, 1, "Nodo 4 ---> 1");
 	add(2, 5, "Nodo 2 ---> 5");
 	add(5, 2, "Nodo 5 ---> 2");
+	add(6, 0, "Nodo 4 ---> 1");
+	add(7, 0, "Nodo 4 ---> 1");
 
 	print();
 
 	int *vect=calloc(MAX_NODE,sizeof(int));
 	int node;
-	
 	printf("Inserire nodo di partenza: ");
 	scanf("%d",&node);
-	
 	vect[0]=node;
+	profondita(node,vect);
+	//getchar();
 
-	ampiezza(node,vect);
-	
-	int i=0;
-	printf("Ampiezza: ");
-	while(vect[i]!=0){
-		printf("%d ",vect[i]);
-
-		i++;
+	int y=0;
+	printf("Profondita`: ");
+	while(vect[y]!=0){
+		printf("%d ",vect[y]);
+		y++;
 	}
 	putchar('\n');
-
+	
+	ampiezza(node,vect);
+	y=0;
+	printf("Ampiezza   : ");
+	while(vect[y]!=0){
+		printf("%d ",vect[y]);
+		y++;
+	}
+	putchar('\n');
 
 	return 0;
 }
